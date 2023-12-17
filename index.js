36711b0e843a61fe7deb7d28737f9833f3b110ec
AklMa7 <fire-2000@hotmail.fr>
2023-12-17 22:51:09 +0100

    Second upload

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 let myLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-l")
+
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
+    
 })
 
+inputEl.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+        myLeads.push(inputEl.value)
+        inputEl.value = ""
+        renderLeads()  
+    }
+})
+
+
+function renderLeads() {
+    let listItems = ''
+    for ( let i = 0 ; i < myLeads.length ; i++) {
+        listItems +=
+        `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+        `
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
+    ulEl.innerHTML = listItems
+
+}
 
 
 
